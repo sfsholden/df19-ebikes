e0ffb1e3f9f88b2be7ffbd60eebf79172aed07b1
Shelby <shelby.holden@salesforce.com>
2019-11-10 21:14:28 -0700

    Added SF commands to task dependencies

--- scripts/init_scratch_org.sh
+++ scripts/init_scratch_org.sh
@@ -5,5 +5,3 @@ sfdx force:source:push
 
 echo -e "\nAssigning perm set 'ebikes' to default scratch org user"
 sfdx force:user:permset:assign -n ebikes
-
-#TODO - also need to refresh sobject definitions. This looked like a good spot.
